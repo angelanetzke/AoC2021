ebaf4fd41f61864dc6848bd2a1062c5329620711
Angela Netzke <36552179+angelanetzke@users.noreply.github.com>
2021-12-02 13:36:52 -0800

    add day 1 and 2

---
+++ Dec01/Dec01/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System;
+using System.IO;
+
+namespace Dec01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Part 1
+            string[] allLines = System.IO.File.ReadAllLines("input.txt");
+            int increaseCount = 0;
+            int lastValue = -1;
+            int currentValue = 0;
+            foreach (string line in allLines)
+            {
+                if (lastValue == -1)
+                {
+                    lastValue = int.Parse(line);
+                }
+                else
+                {
+                    currentValue = int.Parse(line);
+                    if (currentValue > lastValue)
+                    {
+                        increaseCount++;
+                    }
+                    lastValue = currentValue;
+                }
+            }
+            Console.WriteLine("part 1: " + increaseCount);
+
+            // Part 2
+            List<int> allValues = new List<int>();
+            foreach (string line in allLines)
+            {
+                allValues.Add(int.Parse(line));
+            }
+            increaseCount = 0;
+            const int WINDOW_SIZE = 3;
+            int lastStart = 0;
+            int currentStart = 1;
+            int lastWindow = 0;
+            int currentWindow = 0;
+            while (currentStart <= allValues.Count - WINDOW_SIZE)
+            {
+                lastWindow = 0;
+                for (int i = lastStart; i < lastStart + WINDOW_SIZE; i++)
+                {
+                    lastWindow += allValues[i];
+                }
+                currentWindow = 0;
+                for (int i = currentStart; i < currentStart + WINDOW_SIZE; i++)
+                {
+                    currentWindow += allValues[i];
+                }
+                if (currentWindow > lastWindow)
+                {
+                    increaseCount++;
+                }
+                lastStart++;
+                currentStart++;
+            }
+            Console.WriteLine("part 2: " + increaseCount);
+        }
+
+    }
+}
---
+++ Dec02/Dec02/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Dec02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Part 1
+            string[] allLines = System.IO.File.ReadAllLines("input.txt");
+            long horizontalPosition = 0;
+            long verticalPosition = 0;
+            String thisDirection;
+            long thisDistance;
+            foreach (string line in allLines)
+            {
+                string[] tokens = line.Split(" ");
+                thisDirection = tokens[0];
+                thisDistance = long.Parse(tokens[1]);
+                switch (thisDirection)
+                {
+                    case "forward":
+                        horizontalPosition += thisDistance;
+                        break;
+                    case "up":
+                        verticalPosition -= thisDistance;
+                        break;
+                    case "down":
+                        verticalPosition += thisDistance;
+                        break;
+                }
+            }
+            long part1Answer = horizontalPosition * verticalPosition;
+            Console.WriteLine("part 1: " + part1Answer);
+
+            //Part 2
+            long aim = 0l;
+            horizontalPosition = 0l;
+            verticalPosition = 0l;
+            foreach (string thisLine in allLines)
+            {
+                String[] tokens = thisLine.Split(" ");
+                thisDirection = tokens[0];
+                thisDistance = long.Parse(tokens[1]);
+                switch (thisDirection)
+                {
+                    case "forward":
+                        horizontalPosition += thisDistance;
+                        verticalPosition += aim * thisDistance;
+                        break;
+                    case "up":
+                        aim -= thisDistance;
+                        break;
+                    case "down":
+                        aim += thisDistance;
+                        break;
+                }
+            }
+            long part2Answer = horizontalPosition * verticalPosition;
+            Console.Write("part 2: " + part2Answer);
+        }
+
+    }
+}
