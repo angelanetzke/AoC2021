bda7c167dbaefa78e11d3ec6106bc56dad379126
Angela Netzke <36552179+angelanetzke@users.noreply.github.com>
2021-12-05 22:32:00 -0800

    add day 6, part 1

---
+++ Dec06/Dec06/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Dec06
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string[] allLines = File.ReadAllLines("input.txt");
+			List<long> fish = Enumerable.Repeat(0l, 9).ToList();
+			string[] initialValues = allLines[0].Split(",");
+			foreach (string thisValue in initialValues)
+			{
+				fish[int.Parse(thisValue)]++;
+			}			
+			const int TOTAL_DAYS = 80;
+			for (int day = 0; day < TOTAL_DAYS; day++)
+			{
+				long zeroDayFish = fish[0];
+				for (int i = 0; i < fish.Count - 1; i++)
+				{
+					fish[i] = fish[i + 1];
+				}
+				fish[6] += zeroDayFish;
+				fish[8] = zeroDayFish;
+			}
+			Console.WriteLine($"part 1: {fish.Sum()}");
+
+		}
+
+	}
+}
