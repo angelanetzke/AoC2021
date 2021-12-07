ad9389cda4cd24e9b4e0e8f9985db27fe3879381
Angela Netzke <36552179+angelanetzke@users.noreply.github.com>
2021-12-07 06:07:03 -0800

    add day 7, part 1

---
+++ Dec07/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Dec07
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string[] allLines = File.ReadAllLines("input.txt");
+			int[] positions = Array.ConvertAll<string, int>(allLines[0].Split(","), element => int.Parse(element));
+			Array.Sort(positions);
+			int minPosition = positions[0];
+			int maxPosition = positions[positions.Length - 1];
+			Dictionary<int, int> moveCost = new() { };
+			for (int newPosition = minPosition; newPosition <= maxPosition; newPosition++)
+			{
+				int thisNewPositionCost = 0;
+				Array.ForEach<int>(positions, element => thisNewPositionCost += Math.Abs(element - newPosition));
+				moveCost[newPosition] = thisNewPositionCost;
+			}
+			Console.WriteLine($"part 1: {moveCost.Values.Min()}");
+
+		}
+	}
+}
