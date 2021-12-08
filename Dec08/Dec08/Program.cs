15f5d0c7ee81a13268c7d880cb09ae845a49d263
Angela Netzke <36552179+angelanetzke@users.noreply.github.com>
2021-12-08 07:52:22 -0800

    add day 8, part 1

---
+++ Dec08/Dec08/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Dec08
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string[] allLines = File.ReadAllLines("input.txt");
+			int part1Count = 0;
+			foreach (string thisLine in allLines)
+			{
+				string[] outputs = thisLine.Split(" | ")[1].Split(" ");
+				part1Count += outputs.Count(element => element.Length == 2);
+				part1Count += outputs.Count(element => element.Length == 4);
+				part1Count += outputs.Count(element => element.Length == 3);
+				part1Count += outputs.Count(element => element.Length == 7);
+			}
+			Console.WriteLine($"part 1: {part1Count}");
+		}
+	}
+}
