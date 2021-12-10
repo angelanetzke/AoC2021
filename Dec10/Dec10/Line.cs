611a2952e163eee2e1237cd14832305a4b9914dc
Angela Netzke <36552179+angelanetzke@users.noreply.github.com>
2021-12-10 07:16:52 -0800

    add day 10, part 1

---
+++ Dec10/Dec10/Line.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+
+namespace Dec10
+{
+	internal class Line
+	{
+		private string text;
+		private static readonly List<char> leftCharacters = new() { '(', '[', '{', '<' };
+		private static readonly List<char> rightCharacters = new() { ')', ']', '}', '>' };
+		private static readonly Dictionary<char, char> partners = new()
+		{
+			['('] = ')',
+			['['] = ']',
+			['{'] = '}',
+			['<'] = '>'
+		};
+
+		public Line(string text)
+		{
+			this.text = text;
+		}
+
+		public int GetScore()
+		{
+			Stack<char> chars = new();
+			foreach (char thisChar in text)
+			{
+				if (leftCharacters.Contains(thisChar))
+				{
+					chars.Push(thisChar);
+				}
+				else if (rightCharacters.Contains(thisChar))
+				{
+					char topChar = chars.Peek();
+					if (partners[topChar] == thisChar)
+					{
+						chars.Pop();
+					}
+					else
+					{
+						switch (thisChar)
+						{
+							case ')':
+								return 3;
+							case ']':
+								return 57;
+							case '}':
+								return 1197;
+							case '>':
+								return 25137;
+						}
+					}
+				}
+			}
+			return 0;
+		}
+	}
+}
---
+++ Dec10/Dec10/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace Dec10
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string[] allLines = File.ReadAllLines("input.txt");
+			int part1Score = 0;
+			foreach (string thisLine in allLines)
+			{
+				part1Score += (new Line(thisLine).GetScore());
+			}
+			Console.WriteLine($"part 1: {part1Score}");
+		}
+	}
+}
