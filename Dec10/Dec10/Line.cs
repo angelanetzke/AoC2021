959821daebbcef320a7008403ce4905020c402f9
Angela Netzke <36552179+angelanetzke@users.noreply.github.com>
2021-12-10 07:50:40 -0800

    add day 10, part 2

--- Dec10/Dec10/Line.cs
+++ Dec10/Dec10/Line.cs
@@ -20,7 +20,7 @@ namespace Dec10
 			this.text = text;
 		}
 
-		public int GetScore()
+		public int GetPart1Score()
 		{
 			Stack<char> chars = new();
 			foreach (char thisChar in text)
@@ -54,5 +54,48 @@ namespace Dec10
 			}
 			return 0;
 		}
+
+		public long GetPart2Score()
+		{			
+			Stack<char> chars = new();
+			foreach (char thisChar in text)
+			{
+				if (leftCharacters.Contains(thisChar))
+				{
+					chars.Push(thisChar);
+				}
+				else if (rightCharacters.Contains(thisChar))
+				{
+					char topChar = chars.Peek();
+					if (partners[topChar] == thisChar)
+					{
+						chars.Pop();
+					}					
+				}
+			}
+			long score = 0L;
+			while(chars.Count > 0)
+			{
+				char topChar = chars.Pop();
+				switch (topChar)
+				{
+					case '(':
+						score = score * 5L + 1L;
+						break;
+					case '[':
+						score = score * 5L + 2L;
+						break;
+					case '{':
+						score = score * 5L + 3L;
+						break;
+					case '<':
+						score = score * 5L + 4L;
+						break;
+				}
+			}
+			return score;
+		}
+
+
 	}
 }
--- Dec10/Dec10/Program.cs
+++ Dec10/Dec10/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace Dec10
@@ -11,9 +12,22 @@ namespace Dec10
 			int part1Score = 0;
 			foreach (string thisLine in allLines)
 			{
-				part1Score += (new Line(thisLine).GetScore());
+				part1Score += (new Line(thisLine).GetPart1Score());
 			}
 			Console.WriteLine($"part 1: {part1Score}");
+
+			// Part 2
+			List<long> part2Scores = new();
+			foreach (string thisLine in allLines)
+			{
+				Line thisSubsystemLine = new(thisLine);
+				if (thisSubsystemLine.GetPart1Score() == 0)
+				{
+					part2Scores.Add((new Line(thisLine).GetPart2Score()));
+				}
+			}
+			part2Scores.Sort();
+			Console.WriteLine($"part 2: {part2Scores[part2Scores.Count / 2]}");
 		}
 	}
 }
