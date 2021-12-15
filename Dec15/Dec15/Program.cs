3e4bf66fa6fbb6c85f76ed00db98b812d09e466c
Angela Netzke <36552179+angelanetzke@users.noreply.github.com>
2021-12-15 10:19:58 -0800

    add day 15, part 2

--- Dec15/Dec15/Program.cs
+++ Dec15/Dec15/Program.cs
@@ -29,7 +29,29 @@ namespace Dec15
 					theGrid.AddNode(x, y, width, height, costs[y][x]);
 				}
 			}
-			Console.WriteLine($"part 1: {theGrid.GetShortestPath()}"); 
+			Console.WriteLine($"part 1: {theGrid.GetShortestPath()}");
+			int size = 5;
+			int fullWidth = width * size;
+			int fullHeight = height * size;
+			Grid theGrid2 = new();
+			for (int y = 0; y < fullWidth; y++)
+			{
+				for (int x = 0; x < fullHeight; x++)
+				{
+					int gridX = x / width;
+					int gridY = y / height;
+					int costX = x % width;
+					int costY = y % height;
+					int thisCost = costs[costX][costY] + gridX + gridY;
+					if (thisCost > 9)
+					{
+						thisCost -= 9;
+					}
+					theGrid2.AddNode(x, y, fullWidth, fullHeight, thisCost);
+				}
+			}
+			
+			Console.WriteLine($"part 2: {theGrid2.GetShortestPath()}");
 		}
 
 
