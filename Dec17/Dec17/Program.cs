16ad0fbbd58306e3758087d819c6c8c030ea72d1
Angela Netzke <36552179+angelanetzke@users.noreply.github.com>
2021-12-17 08:40:01 -0800

    add day 17, part 1

---
+++ Dec17/Dec17/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Dec17
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string inputString = File.ReadAllText("input.txt");
+			var regexY = Regex.Match(inputString, @"y=(?<yvalues>-*\d+..-*\d+)");
+			int depth = 0 - int.Parse(regexY.Groups["yvalues"].Value.Split("..")[0]);
+			int maxHeight = (depth - 1) * depth / 2;
+			Console.WriteLine($"part 1: {maxHeight}");
+
+
+
+		}
+	}
+}
