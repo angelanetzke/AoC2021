f7b78b7d7e5fb5e2b78f9dd5193179278dd2077b
Angela Netzke <36552179+angelanetzke@users.noreply.github.com>
2021-12-20 11:01:54 -0800

    add day 20, part 1

---
+++ Dec20/Dec20/Image.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dec20
+{
+	internal class Image
+	{
+		private readonly string algorithm;
+		private HashSet<(int, int)> lights;
+		private int minX;
+		private int maxX;
+		private int minY;
+		private int maxY;
+		private char outerChar;
+
+		public Image(string algorithm, List<string> initialImage)
+		{
+			this.algorithm = algorithm;
+			outerChar = '.';
+			lights = new();
+			minX = 0;
+			maxX = initialImage[0].Length - 1;
+			minY = 0;
+			maxY = initialImage.Count - 1;
+			for (int y = minY; y <= maxY; y++)
+			{
+				for (int x = minX; x <= maxX; x++)
+				{
+					if (initialImage[y][x] == '#')
+					{
+						lights.Add((x, y));
+					}
+				}
+			}
+		}
+
+		public void Enhance()
+		{
+			HashSet<(int, int)> newLights = new();
+			minX--;
+			maxX++;
+			minY--;
+			maxY++;
+			for (int x = minX; x <= maxX; x++)
+			{
+				for (int y = minY; y <= maxY; y++)
+				{
+					if (IsLight(x, y))
+					{
+						newLights.Add((x, y));
+					}
+				}
+			}
+			if (outerChar == '.')
+			{
+				outerChar = algorithm[0];
+			}
+			else
+			{
+				outerChar = algorithm[algorithm.Length - 1];
+			}
+			lights = newLights;
+		}
+
+
+		private bool IsLight(int x, int y)
+		{
+			StringBuilder builder = new();
+			for (int squareY = y - 1; squareY <= y + 1; squareY++)
+			{
+				for (int squareX = x - 1; squareX <= x + 1; squareX++)
+				{
+					if (squareX <= minX || squareX >= maxX || squareY <= minY || squareY >= maxY)
+					{
+						if (outerChar == '#')
+						{
+							builder.Append('1');
+						}
+						else
+						{
+							builder.Append('0');
+						}						
+					}
+					else
+					{
+						if (lights.Contains((squareX, squareY)))
+						{
+							builder.Append('1');
+						}
+						else
+						{
+							builder.Append('0');
+						}
+					}
+				}
+			}
+			int index = Convert.ToInt32(builder.ToString(), 2);
+			return algorithm[index] == '#';
+		}
+
+		public int CountLights()
+		{
+			return lights.Count;
+		}
+
+
+	}
+}
---
+++ Dec20/Dec20/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Dec20
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string[] allLines = File.ReadAllLines("input.txt");
+			string algorithm = allLines[0];
+			List<string> initialImage = new();
+			for (int i = 2; i < allLines.Length; i++)
+			{
+				initialImage.Add(allLines[i]);
+			}
+			Image theImage = new(algorithm, initialImage);
+			theImage.Enhance();
+			theImage.Enhance();
+			Console.WriteLine($"part 1: {theImage.CountLights()}");
+		}
+
+
+	}
+}
