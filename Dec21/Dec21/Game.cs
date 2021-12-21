d16fe67205816daed6bed68ba03e0d4c50291816
Angela Netzke <36552179+angelanetzke@users.noreply.github.com>
2021-12-21 12:38:30 -0800

    add day 21, part 1

---
+++ Dec21/Dec21/Game.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dec21
+{
+	internal class Game
+	{
+		private int nextRoll;
+		private int player1Position;
+		private int player2Position;
+		int player1Score;
+		int player2Score;
+		private bool isPlayer1Turn;
+		private int rollCount;
+
+		public Game(int player1Position, int player2Position)
+		{
+			nextRoll = 1;
+			this.player1Position = player1Position;
+			this.player2Position = player2Position;
+			player1Score = 0;
+			player2Score = 0;
+			isPlayer1Turn = true;
+			rollCount = 0;
+		}
+
+		public void Next()
+		{
+			int thisRoll = nextRoll * 3 + 3;
+			nextRoll += 3;
+			if (isPlayer1Turn)
+			{
+				player1Position = (player1Position + thisRoll - 1) % 10 + 1;
+				player1Score += player1Position;
+			}
+			else
+			{
+				player2Position = (player2Position + thisRoll - 1) % 10 + 1;
+				player2Score += player2Position;
+			}
+			rollCount += 3;
+			isPlayer1Turn = !isPlayer1Turn;
+		}
+
+		public int GetPlayer1Score()
+		{
+			return player1Score;
+		}
+
+		public int GetPlayer2Score()
+		{
+			return player2Score;
+		}
+
+		public int GetRollCount()
+		{
+			return rollCount;
+		}
+
+
+	}
+}
---
+++ Dec21/Dec21/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace Dec21
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string[] allLines = File.ReadAllLines("input.txt");
+			int player1Start = int.Parse(allLines[0].Split(": ")[1]);
+			int player2Start = int.Parse(allLines[1].Split(": ")[1]);
+			Game theGame = new (player1Start, player2Start);
+			while (theGame.GetPlayer1Score() < 1000 && theGame.GetPlayer2Score() < 1000)
+			{
+				theGame.Next();
+			}
+			long part1Answer;
+			if (theGame.GetPlayer1Score() < theGame.GetPlayer2Score())
+			{
+				part1Answer = theGame.GetPlayer1Score() * theGame.GetRollCount();
+			}
+			else
+			{
+				part1Answer = theGame.GetPlayer2Score() * theGame.GetRollCount();
+			}
+			Console.WriteLine($"part 1: {part1Answer}");
+		}
+
+
+
+	}
+}
